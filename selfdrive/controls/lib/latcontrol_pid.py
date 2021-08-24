2930d75889eaf12e1cb8fd0383f1b089486ec3b7
Comma Device <device@comma.ai>
2021-08-24 01:43:15 -0700

    initial 0.8.8

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -34,7 +34,8 @@ class LatControlPID():
 
       # TODO: feedforward something based on lat_plan.rateSteers
       steer_feedforward = angle_steers_des_no_offset  # offset does not contribute to resistive torque
-      steer_feedforward *= CS.vEgo**2  # proportional to realigning tire momentum (~ lateral accel)
+      # !!! FOR VOLT ONLY !!! GM steering accounts for vEgo in the torque command.
+      steer_feedforward *= CS.vEgo  # proportional to realigning tire momentum (~ lateral accel)
 
       deadzone = 0.0
 
