9e8e3b8dcfceaed54894c2b635715aa8256cd094
Comma Device <device@comma.ai>
2021-09-02 22:13:46 -0700

    update to qadmus sigmoid tune

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -12,10 +12,6 @@ EventName = car.CarEvent.EventName
 class CarInterface(CarInterfaceBase):
 
   @staticmethod
-  def compute_gb(accel, speed):
-    return float(accel) / 4.0
-
-  @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=None):
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
     ret.carName = "gm"
@@ -54,7 +50,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate == CAR.VOLT:
       # supports stop and go, but initial engage must be above 18 mph
-      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.minEnableSpeed = 3 * CV.MPH_TO_MS
       ret.mass = 1607. + STD_CARGO_KG
       ret.wheelbase = 2.69
       ret.steerRatio = 17.7  # Stock 15.7, LiveParameters
@@ -63,14 +59,14 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 0.45 * ret.wheelbase # from Volt Gen 1
 
       ret.lateralTuning.pid.kpBP = [0., 40.]
-      ret.lateralTuning.pid.kpV = [0., 0.2] # kp linear with speed
+      ret.lateralTuning.pid.kpV = [0., 0.2] # kp proportional to velocity
       ret.lateralTuning.pid.kiBP = [0.]
       ret.lateralTuning.pid.kiV = [0.005] # ki constant
-      ret.lateralTuning.pid.kf = 0.0022 # !!! ONLY for (angle * vEgo) feedforward !!!
-      ret.steerRateCost = 0.5 # TODO
-      ret.steerActuatorDelay = 0.21 # carControl.actuators to actuator torque
+      ret.lateralTuning.pid.kf = 0.1 # !!! ONLY for Volt sigmoid feedforward !!!
+      ret.steerRateCost = 0.5 # TODO, this probably varies with speed.
+      ret.steerActuatorDelay = 0.21 # measured from actuators.steer to carState.steeringTorqueEps
 
-      # Only tuned to reduce oscillations. TODO.
+      # TODO, only tuned to reduce overshoot.
       ret.longitudinalTuning.kpV = [1.7, 1.3]
       ret.longitudinalTuning.kiV = [0.36]
 
