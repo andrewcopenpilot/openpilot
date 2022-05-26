fb97434079a8511c2041fe424611c5811ee815a8
Comma Device <device@comma.ai>
2022-05-25 21:42:10 -0700

    Acutally add patched files

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -16,7 +16,8 @@ EventName = car.CarEvent.EventName
 
 
 def get_startup_event(car_recognized, controller_available, fw_seen):
-  if is_comma_remote() and is_tested_branch():
+  #if is_comma_remote() and is_tested_branch():
+  if True:
     event = EventName.startup
   else:
     event = EventName.startupMaster
@@ -177,6 +178,7 @@ def fingerprint(logcan, sendcan):
 def get_car(logcan, sendcan):
   candidate, fingerprints, vin, car_fw, source, exact_match = fingerprint(logcan, sendcan)
 
+  candidate = "CHEVROLET VOLT PREMIER 2017"
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
     candidate = "mock"
