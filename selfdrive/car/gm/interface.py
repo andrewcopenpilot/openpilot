bcaa6052e448c02cfeef4ef296ce14b5d2373e47
Comma Device <device@comma.ai>
2023-01-13 18:30:03 -0800

    Initial 0.9

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -113,6 +113,7 @@ class CarInterface(CarInterfaceBase):
     ret.radarTimeStep = 0.0667  # GM radar runs at 15Hz instead of standard 20Hz
 
     if candidate == CAR.VOLT:
+      ret.minEnableSpeed = 3 * CV.MPH_TO_MS
       ret.mass = 1607. + STD_CARGO_KG
       ret.wheelbase = 2.69
       ret.steerRatio = 17.7  # Stock 15.7, LiveParameters
