24f7b68c050b9b66ed8ecc0e6096034821167dfc
Comma Device <device@comma.ai>
2023-12-11 01:54:21 -0800

    Initial 0.9.5 release

---
+++ github_init_ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir ~/.ssh
+cat <<EOF > ~/.ssh/config
+Host github.com
+  User git
+  Hostname github.com
+  PreferredAuthentications publickey
+  IdentityFile /data/id_rsa
+EOF
--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+export MAPBOX_TOKEN=`cat /data/mapboxapikey`
+export ZMQ=1
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
