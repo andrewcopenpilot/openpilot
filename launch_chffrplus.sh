a778a85b1b519988db167a01019a2c1b2895b144
Comma Device <device@comma.ai>
2025-08-24 21:19:01 -0700

    Log tmux to file

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -82,7 +82,8 @@ function launch {
   if [ ! -f $DIR/prebuilt ]; then
     ./build.py
   fi
-  ./manager.py
+  LOG_FILE="/data/tmux-logs-$(date -u +'%Y-%m-%d--%H-%M-%S').log"
+  ./manager.py > "$LOG_FILE" 2>&1
 
   # if broken, keep on screen error
   while true; do sleep 1; done
